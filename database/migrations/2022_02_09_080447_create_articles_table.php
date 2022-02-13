4a4cbaa321f5dc14e30d5b48040654975681e6b0
bodia <v.bodyansky@yandex.ru>
2022-02-13 19:56:40 +1000

    change type content column

--- database/migrations/2022_02_09_080447_create_articles_table.php
+++ database/migrations/2022_02_09_080447_create_articles_table.php
@@ -16,7 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('content');
+            $table->text('content');
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
