3159ff39bb32a70b89b9b049f72a0d18e5e9bc93
bodia <v.bodyansky@yandex.ru>
2022-02-15 17:42:06 +1000

    display popular articles

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,30 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="col-lg-8 mx-auto">
-        @if (Session::has('success'))
-            <div class="alert alert-success" role="alert">
-                {{ Session::get('success')  }}
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                @if (session('success'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('success')  }}
+                    </div>
+                @endif
+    
+                @foreach($articles as $article)
+                    <div class="card my-3">
+                        <div class="card-header">
+                            <a class="link-secondary" href="{{ route('articles.show', $article) }}">
+                                <strong>{{ $article->title }}</strong>
+                            </a>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                {{ $article->content }}
+                            </p>
+                        </div>
+                        <div class="card-footer p-1">
+                            <p class="card-text text-end"><small>{{ $article->user->name }}</small></p>
+                        </div>
+                    </div>
+                @endforeach
+                {{ $articles->links() }}
             </div>
-        @endif
-
-            @foreach($articles as $article)
+            <div class="col-lg-4">
                 <div class="card my-3">
                     <div class="card-header">
-                        <a class="link-secondary" href="{{ route('articles.show', $article) }}">
-                            <strong>{{ $article->title }}</strong>
-                        </a>
+                        <strong>Most Popular Articles</strong>
                     </div>
                     <div class="card-body">
-                        <p class="card-text">
-                            {{ $article->content }}
-                        </p>
-                    </div>
-                    <div class="card-footer p-1">
-                        <p class="card-text text-end"><small>{{ $article->user->name }}</small></p>
+                        @foreach ($popularArticles as $popularArticle)
+                            <a href="{{ route('articles.show', $popularArticle) }}">{{ $popularArticle->title }}</a>
+                            <small>{{ $popularArticle->likes_count }} likes</small>
+                            @if (!$loop->last)
+                                <hr>
+                            @endif
+                        @endforeach
                     </div>
                 </div>
-            @endforeach
-            {{ $articles->links() }}
+            </div>
+        </div>
     </div>
 @endsection
