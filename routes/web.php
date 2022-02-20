dfa254db356972256d55b6c744f4132bff3bcb69
bodianskii <v.bodyansky@yandex.ru>
2022-02-20 19:34:16 +1000

    registered routes for user

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,15 @@ use App\Http\Controllers\ArticleCommentController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ArticleLikeController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::resource('users', UserController::class)->only('show', 'edit', 'update');
 
 Route::resource('articles', ArticleController::class)
     ->scoped(['article' => 'slug'])
